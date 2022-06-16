b88f3104d0f2d72f87523c3e85de05f960424d2e
CharleneMSI <q2330416@gmail.com>
2022-06-16 12:03:50 +0800

    Main code

---
+++ MalignantAnnotation_Main.R
@@ -0,0 +1,78 @@
+##### Presetting ######
+  rm(list = ls()) # Clean variable
+  memory.limit(150000)
+
+##### Current path and new folder setting* #####
+  ProjectName = "MaliAnno"
+  Sampletype = "PDAC"
+  #ProjSamp.Path = paste0(Sampletype,"_",ProjectName)
+
+  Version = paste0(Sys.Date(),"_",ProjectName,"_",Sampletype)
+  Save.Path = paste0(getwd(),"/",Version)
+  ## Create new folder
+  if (!dir.exists(Save.Path)){
+    dir.create(Save.Path)
+  }
+
+##### Load Packages #####
+  #### Basic installation ####
+    Package.set <- c("tidyverse","Seurat","ggplot2","ggpmisc",
+                     "stringr","magrittr","dplyr")
+    ## Check whether the installation of those packages is required from basic
+    for (i in 1:length(Package.set)) {
+      if (!requireNamespace(Package.set[i], quietly = TRUE)){
+        install.packages(Package.set[i])
+      }
+    }
+    ## Load Packages
+    lapply(Package.set, library, character.only = TRUE)
+    rm(Package.set,i)
+
+  #### BiocManager installation ####
+    ## Check whether the installation of those packages is required from BiocManager
+    if (!require("BiocManager", quietly = TRUE))
+      install.packages("BiocManager")
+    Package.set <- c("fgsea","AnnotationHub","ensembldb",
+                     "SeuratDisk",
+                     "SingleR","scRNAseq","celldex","scran")
+    for (i in 1:length(Package.set)) {
+      if (!requireNamespace(Package.set[i], quietly = TRUE)){
+        BiocManager::install(Package.set[i])
+      }
+    }
+    ## Load Packages
+    lapply(Package.set, library, character.only = TRUE)
+    rm(Package.set,i)
+
+    options(stringsAsFactors = FALSE)
+
+  #### GitHub installation ####
+    if (!require("devtools", quietly = TRUE))
+      install.packages("devtools")
+    devtools::install_github("cole-trapnell-lab/garnett")
+    devtools::install_github('cole-trapnell-lab/monocle3')
+    devtools::install_github("LTLA/SingleR")
+
+    library(monocle3)
+    library(garnett)
+    # library(SingleR)
+
+#### Load data #####
+    load("SeuratObject_PRJCA001063.RData")
+
+#### Plot UMAP #####
+    FeaturePlot(seuratObject, features = c("MS4A1", "GNLY", "CD3E", "CD14"))
+    Idents(seuratObject) <- seuratObject@meta.data[["Cell_type"]]
+    DimPlot(seuratObject, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+
+    # ReDR
+    seuratObject2 <- FindVariableFeatures(seuratObject, selection.method = "vst", nfeatures = 2000)
+    seuratObject2 <- RunPCA(seuratObject2,npcs = 200, features = VariableFeatures(object = seuratObject2))
+    seuratObject2 <- FindNeighbors(seuratObject2, dims = 1:100)
+    seuratObject2 <- FindClusters(seuratObject2, resolution = 0.5)
+    seuratObject2 <- RunUMAP(seuratObject2, dims = 1:100,n.neighbors = 15)
+    DimPlot(seuratObject, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+
+
+
+
