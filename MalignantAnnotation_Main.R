d7cb1d2c6deca4cec5f3ccf99a9d10ce5747dd7d
CharleneMSI <q2330416@gmail.com>
2022-06-18 16:37:11 +0800

    seuratObject setting

--- MalignantAnnotation_Main.R
+++ MalignantAnnotation_Main.R
@@ -33,7 +33,7 @@
     if (!require("BiocManager", quietly = TRUE))
       install.packages("BiocManager")
     Package.set <- c("fgsea","AnnotationHub","ensembldb",
-                     "SeuratDisk",
+                     "SeuratDisk","monocle",
                      "SingleR","scRNAseq","celldex","scran")
     for (i in 1:length(Package.set)) {
       if (!requireNamespace(Package.set[i], quietly = TRUE)){
@@ -49,6 +49,7 @@
   #### GitHub installation ####
     if (!require("devtools", quietly = TRUE))
       install.packages("devtools")
+    library(monocle)
     devtools::install_github("cole-trapnell-lab/garnett")
     devtools::install_github('cole-trapnell-lab/monocle3')
     devtools::install_github("LTLA/SingleR")
@@ -69,12 +70,14 @@
     row.names(seurat_meta.df) <- seurat_meta.df[,1]
     seuratObject@meta.data <- seurat_meta.df
 
+    seuratObjectMono_Ori <- seuratObject
+
     library("stringr")
     rm(list=setdiff(ls(), str_subset(objects(), pattern = "seuratObject")))
     save.image("SeuratObject_CDS_PRJCA001063.RData")
 
 # #### Load data #####
-#     load("SeuratObject_CDS_PRJCA001063.RData")
+     load("SeuratObject_CDS_PRJCA001063.RData")
 
 #### Plot UMAP #####
     FeaturePlot(seuratObject, features = c("MS4A1", "GNLY", "CD3E", "CD14"))
@@ -84,14 +87,35 @@
 
     # ReDR
     seuratObject <- FindVariableFeatures(seuratObject, selection.method = "vst", nfeatures = 2000)
+    seuratObject <- FindVariableFeatures(seuratObject)
     seuratObject <- RunPCA(seuratObject,npcs = 200, features = VariableFeatures(object = seuratObject))
     seuratObject <- FindNeighbors(seuratObject, dims = 1:100)
     seuratObject <- FindClusters(seuratObject, resolution = 0.5)
-    seuratObject <- RunUMAP(seuratObject, dims = 1:100,n.neighbors = 15)
+    seuratObject <- RunUMAP(seuratObject, dims = 1:100,n.neighbors = 20, min.dist=0.3)
+    # seuratObject <- RunUMAP(seuratObject, dims = 1:100,n.neighbors = 1000, min.dist=0.1)
+    # seuratObject@meta.data[["UMAP_NNei1000"]] <- seuratObject@reductions[["umap"]]@cell.embeddings
+    # seuratObject@meta.data <- seuratObject@meta.data[,!colnames(seuratObject@meta.data)=="UMAP_NNei1000"]
+    seuratObject@meta.data[["UMAP_NNei20_MD03"]] <- seuratObject@reductions[["umap"]]@cell.embeddings
+
     DimPlot(seuratObject, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
 
+    Idents(seuratObject) <- seuratObject@meta.data[["Cell_type"]]
+    DimPlot(seuratObject, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
 
-#### Plot UMAP #####
+    Idents(seuratObject) <- seuratObject@meta.data[["ReCluster"]]
+    DimPlot(seuratObject, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
+    FeaturePlot(seuratObject, features = c("TOP2A"))
+
+#### Cell type markers #####
+    ## Create cell type markers dataframe
+    # DefaultAssay(scRNA.SeuObj_Small) <- "RNA"
+    Idents(seuratObject) <- seuratObject@meta.data[["ReCluster"]]
+    ReCellType.markers <- FindAllMarkers(seuratObject, only.pos = TRUE, min.pct = 0.1, logfc.threshold = 0.25)
+
+    write.table(ReCellType.markers, file = paste0( Save.Path,"/ReCelltypeMarker2_AllGene.txt"),
+                quote = F,sep = "\t",row.names = F)
+    save.image("SeuratObject_CDS_PRJCA001063_MaligAnno.RData")
+
 
 
 
