3694fc1c4eb22be3ea1b696d32baab617478e1d9
CharleneMSI <q2330416@gmail.com>
2022-06-28 13:42:19 +0800

    Clean up the code.

--- CellTypeAnno_scSorter_ForFUN.R
+++ CellTypeAnno_scSorter_ForFUN.R
@@ -78,13 +78,21 @@
                     top_n(n = 10, wt = avg_log2FC)
 
   #DoHeatmap(scRNA.SeuObj, features = CTTop.markers$gene) + NoLegend()
-  anno.df <- data.frame(Type = CTTop.markers$cluster,
-                        Marker = CTTop.markers$gene,
-                        Weight = CTTop.markers$avg_log2FC)
+  log2FC_CN <- "avg_log2FC"
+  Gene_CN <- "gene"
+  Cluster_CN <- "cluster"
+
+  anno.df <- data.frame(Type = CTFilter.Markers.df[,Cluster_CN],
+                        Marker = CTFilter.Markers.df[,Gene_CN],
+                        Weight = CTFilter.Markers.df[,log2FC_CN])
+
+##### Set Target Gene expression matrix #####
+  GeneExp.df <- GetAssayData(scRNA.SeuObj, assay = "RNA", slot = "data") # normalized data matrix
+  # GeneExp.df <- scRNA.SeuObj@assays[["RNA"]]@counts %>% as.data.frame()
 
 #### Run scSorter ####
   library(scSorter)
-  scSorter.obj <- scSorter(scRNA.SeuObj@assays[["RNA"]]@counts %>% as.data.frame(),anno.df)
+  scSorter.obj <- scSorter(GeneExp.df,anno.df)
 
   scRNA.SeuObj$scSorterPred <- scSorter.obj[["Pred_Type"]]
   p.CTPred1 <- DimPlot(scRNA.SeuObj, reduction = "umap", group.by ="scSorterPred" ,label = TRUE, pt.size = 0.5) + NoLegend()
@@ -96,42 +104,10 @@
   p.CTComp1 <- ggarrange(p.CT1, p.CTPred1, common.legend = TRUE, legend = "top")
   p.CTComp1
 
-
-
-
-
-
-
-##### Section 1 - Preliminaries #####
-  library(scSorter)
-
-##### Section 2 - Preprocessing the data #####
-  ## Create anno.df
-  # DoHeatmap(scRNA.SeuObj, features = CTFilter.Markers.df$gene) + NoLegend()
-  log2FC_CN <- "avg_log2FC"
-  Gene_CN <- "gene"
-  Cluster_CN <- "cluster"
-
-  anno.df <- data.frame(Type = CTFilter.Markers.df[,Cluster_CN],
-                        Marker = CTFilter.Markers.df[,Gene_CN],
-                        Weight = CTFilter.Markers.df[,log2FC_CN])
-
-  # anno.df <- data.frame(Type = CTFilter.Markers.df$cluster,
-  #                       Marker = CTFilter.Markers.df$gene,
-  #                       Weight = CTFilter.Markers.df$avg_log2FC)
-
-  ## Export Gene expression matrix
-  GeneExp.df <- GetAssayData(scRNA.SeuObj, assay = "RNA", slot = "data") # normalized data matrix
-
-##### Section 3 - Running scSorter #####
-  scSorter.obj <- scSorter(GeneExp.df, anno.df)
-
-  ## Insert the scSOrter data to scRNA.SeuObj
-  # scRNA.SeuObj$scSorterPred <- scSorter.obj[["Pred_Type"]]
-  # DimPlot(scRNA.SeuObj, reduction = "umap", group.by ="scSorterPred" ,label = TRUE, pt.size = 0.5) + NoLegend()
-  # DimPlot(scRNA.SeuObj, reduction = "umap", group.by ="celltype" ,label = TRUE, pt.size = 0.5) + NoLegend()
-
   scRNA.SeuObj@meta.data[[paste0("scSorterPred","_LogFC",1,"_pV",0.05)]] <- scSorter.obj[["Pred_Type"]]
   DimPlot(scRNA.SeuObj, reduction = "umap",
           group.by = paste0("scSorterPred","_LogFC",1,"_pV",0.05),label = TRUE, pt.size = 0.5) + NoLegend()
   DimPlot(scRNA.SeuObj, reduction = "umap", group.by ="celltype" ,label = TRUE, pt.size = 0.5) + NoLegend()
+
+
+
